8d1ea768a8e94ad025455f800388894577db7959
Debora Cervieri Guterres <debora.guterres@gmail.com>
2022-01-04 14:02:37 -0300

    Developed DELETE BY ID functionality

--- src/main/java/com/dio/personapi/controller/PersonController.java
+++ src/main/java/com/dio/personapi/controller/PersonController.java
@@ -6,11 +6,13 @@ import javax.validation.Valid;
 
 import com.dio.personapi.dto.MessageResponseDTO;
 import com.dio.personapi.dto.request.PersonDTO;
+import com.dio.personapi.entity.Person;
 import com.dio.personapi.exception.PersonNotFoundException;
 import com.dio.personapi.service.PersonService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -47,4 +49,10 @@ public class PersonController {
     public PersonDTO findById(@PathVariable Long id) throws PersonNotFoundException {
         return personService.findById(id);
     }
-}
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteById(@PathVariable Long id) throws PersonNotFoundException {
+        personService.delete(id);
+    }
+
